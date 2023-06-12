18a7fde65e87e8787dcb546ccef447056b474186
DoBac2004 <111523003+DoBac2004@users.noreply.github.com>
2023-06-12 14:53:28 +0700

    Add files via upload

---
+++ project1/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Car
+    {
+		public string make;
+		public string model;
+		public string color;
+		public int yearBuilt;
+
+		public void Strart()
+		{
+			System.Console.WriteLine(model + "started");
+		}
+		public void Stop()
+		{
+			System.Console.WriteLine(model + "stopped");
+		}
+	}
+}
